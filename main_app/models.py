4edb4caa03cfcba7a260e59a1448fbeff67b628f
Mahesh.N.D <maheshndevraj@gmail.com>
2024-05-08 17:57:29 +0530

    Your msg

---
+++ main_app/models.py
@@ -0,0 +1,166 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import UserManager
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class customuserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = customuser(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        assert extra_fields["is_staff"]
+        assert extra_fields["is_superuser"]
+        return self._create_user(email, password, **extra_fields)
+
+class customuser(AbstractUser):
+    USER_TYPE = ((1, "CEO"), (2, "Manager"), (3, "Employee"))
+    GENDER = [("M", "Male"), ("F", "Female")]
+    
+    username = None  # Removed username, using email instead
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(default=1, choices=USER_TYPE, max_length=1)
+    gender = models.CharField(max_length=1, choices=GENDER)
+    profile_pic = models.ImageField()
+    address = models.TextField()
+    fcm_token = models.TextField(default="")  # For firebase notifications
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = customuserManager()
+
+    def __str__(self):
+        return self.last_name + ", " + self.first_name
+
+class Admin(models.Model):
+    admin = models.OneToOneField(customuser, on_delete=models.CASCADE, related_name='admin_user')
+    id = models.BigAutoField(primary_key=True)  # Add this line
+
+class Division(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    id = models.BigAutoField(primary_key=True)  # Add this line
+
+    def __str__(self):
+        return self.name
+
+class Manager(models.Model):
+    division = models.ForeignKey(Division, on_delete=models.DO_NOTHING, null=True, blank=False)
+    admin = models.OneToOneField(customuser, on_delete=models.CASCADE, related_name='manager_user')
+
+    def __str__(self):
+        return self.admin.last_name + " " + self.admin.first_name
+
+class Department(models.Model):
+    name = models.CharField(max_length=120)
+    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    admin = models.OneToOneField(customuser, on_delete=models.CASCADE, related_name='employee_user')
+    division = models.ForeignKey(Division, on_delete=models.DO_NOTHING, null=True, blank=False)
+    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING, null=True, blank=False)
+
+    def __str__(self):
+        return self.admin.last_name + ", " + self.admin.first_name
+
+class Attendance(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class AttendanceReport(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.DO_NOTHING)
+    attendance = models.ForeignKey(Attendance, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class LeaveReportEmployee(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.CharField(max_length=60)
+    message = models.TextField()
+    status = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class LeaveReportManager(models.Model):
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    date = models.CharField(max_length=60)
+    message = models.TextField()
+    status = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FeedbackEmployee(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FeedbackManager(models.Model):
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class NotificationManager(models.Model):
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class NotificationEmployee(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class EmployeeSalary(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    base = models.FloatField(default=0)
+    ctc = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+@receiver(post_save, sender=customuser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.user_type == 1:
+            Admin.objects.create(admin=instance)
+        if instance.user_type == 2:
+            Manager.objects.create(admin=instance)
+        if instance.user_type == 3:
+            Employee.objects.create(admin=instance)
+
+@receiver(post_save, sender=customuser)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == 1:
+        instance.admin_user.save()
+    if instance.user_type == 2:
+        instance.manager_user.save()
+    if instance.user_type == 3:
+        instance.employee_user.save()
